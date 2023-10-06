3ea9b16a14b721d76958fead09e4e4f0f7036ceb
Josh Olds <joshua.olds@outlook.com>
2023-10-05 21:50:41 -0600

    Adds weapon_modifiers and multiple projectile handling to gun manager

--- Scenes/guns/gun_manager/gun_manager.gd
+++ Scenes/guns/gun_manager/gun_manager.gd
@@ -22,6 +22,15 @@ extends Node2D
 ## Timer used to update gun cooldown
 var _cooldown_timer : CooldownTimer
 
+# --------- Gun Defaults ----------
+## Default spread value (rad). Used for seperation angle between multiple projectiles in standard guns
+var spread : float = PI / 18.0
+
+# --------- Modifiers -------------
+@export var multiple_projectiles_mod : int = 1
+
+var spread_mod : float = 1.0
+
 
 func _ready():
 	# set up cooldown timer
@@ -30,7 +39,6 @@ func _ready():
 	add_child(_cooldown_timer)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	if Input.is_action_pressed("mouse_left_click"):
 		shoot()
@@ -42,15 +50,12 @@ func shoot():
 		# Go on cooldown and set timer
 		_cooldown_timer.wait_time = 1.0 / fire_rate
 		_cooldown_timer.start_cooldown()
-		spawn_bullet()
+		spawn_bullets()
 		
 	
-# Spawns a bullet. Instantiates bullet scene and applys velocity and forces	
-func spawn_bullet():
+## Instantiates a bullet. Applies impulse to bullet
+func spawn_bullet(spawn_position : Vector2, spawn_rotation : float):
 	var bullet = bullet_scene.instantiate() as BasicBullet
-	
-	var spawn_position = $BulletSpawnLocation.global_position
-	var spawn_rotation = $BulletSpawnLocation.global_rotation
 	var spawn_velocity : Vector2 = spawn_rigid_body.linear_velocity
 	var forward_vec =  Vector2(cos(spawn_rotation), sin(spawn_rotation))
 	var spawn_offset_position : Vector2 = spawn_position + (bullet.radius * forward_vec)
@@ -60,3 +65,41 @@ func spawn_bullet():
 	bullet.bullet_manager = bullet_manager
 	bullet.apply_central_impulse(forward_vec * bullet_spawn_impulse)
 	bullet_manager.add_child(bullet)
+
+
+## Spawns bullets accounting for multiple projectiles modifier. Calls spawn_bullet() per bullet instantiated
+func spawn_bullets():
+	var projectile_count : int = multiple_projectiles_mod
+	var projectile_seperation = spread * spread_mod
+	var original_spawn_position = $BulletSpawnLocation.global_position
+	var parent_to_spawn_vector = original_spawn_position - global_position
+
+	# Spawn the first projectile outside of loop, since first is handled a bit differently
+	# If Odd number of projectiles, one fires down the middle. If Even, they are offset
+	var first_bullet_angle_offset = 0
+	if projectile_count > 1:
+		if projectile_count % 2 == 0:
+			first_bullet_angle_offset = -1 * projectile_seperation / 2
+
+	var rotated_parent_to_spawn_vector = parent_to_spawn_vector.rotated(first_bullet_angle_offset)
+	var spawn_position = global_position + (rotated_parent_to_spawn_vector)
+	var spawn_rotation = global_rotation + first_bullet_angle_offset
+	spawn_bullet(spawn_position, spawn_rotation)
+
+	# Spawn the rest of the bullets in a loop. Increment angle offset each iteration
+	var left_angle_offset = 0
+	var right_angle_offset = 0
+	for x in projectile_count - 1:
+		if x % 2 == 0:
+			right_angle_offset += projectile_seperation
+			rotated_parent_to_spawn_vector = parent_to_spawn_vector.rotated(right_angle_offset + first_bullet_angle_offset)
+			spawn_position = global_position + (rotated_parent_to_spawn_vector)
+			spawn_rotation = global_rotation + right_angle_offset + first_bullet_angle_offset
+			spawn_bullet(spawn_position, spawn_rotation)
+		else:
+			left_angle_offset -= projectile_seperation
+			rotated_parent_to_spawn_vector = parent_to_spawn_vector.rotated(left_angle_offset + first_bullet_angle_offset)
+			spawn_position = global_position + (rotated_parent_to_spawn_vector)
+			spawn_rotation = global_rotation + left_angle_offset + first_bullet_angle_offset
+			spawn_bullet(spawn_position, spawn_rotation)
+				
---
+++ Scenes/systems/weapon_modifiers/weapon_modifiers.gd
@@ -0,0 +1,49 @@
+class_name WeaponModifiers
+extends Node
+
+## Emitted any time a modifier value changes
+signal weapon_modifiers_changed
+
+
+## Multiplier for mass of a projectile
+var mass_mod : float = 1.0 :
+	set(new_value):
+		mass_mod = new_value
+		weapon_modifiers_changed.emit()
+
+## Multiplier for damage of a projectile
+var damage_mod : float = 1.0 :
+	set(new_value):
+			damage_mod = new_value
+			weapon_modifiers_changed.emit()
+
+
+## Multiplier for size of a projectile
+var size_mod : float = 1.0 :
+	set(new_value):
+			size_mod = new_value
+			weapon_modifiers_changed.emit()
+
+
+## -------------- Gun Modifiers -----------------
+
+## Multiplier for cooldown time of a gun
+var cooldown_mod : float = 1.0 :
+	set(new_value):
+			cooldown_mod = new_value
+			weapon_modifiers_changed.emit()
+
+## Multiplier for multiple projectiles fired from a gun
+var multiple_projectiles_mod : int = 1 :
+	set(new_value):
+			multiple_projectiles_mod = new_value
+			weapon_modifiers_changed.emit()
+
+
+## Multiplier for the spread of projectiles fired from a gun
+var spread_mod : float = 1.0 :
+	set(new_value):
+			spread_mod = new_value
+			weapon_modifiers_changed.emit()
+
+
