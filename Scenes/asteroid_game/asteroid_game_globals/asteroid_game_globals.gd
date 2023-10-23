15664ff399eea380974b87d0de3f0a9504220c46
Josh Olds <joshua.olds@outlook.com>
2023-10-23 17:45:28 -0600

    removes capital Scenes folder

--- Scenes/asteroid_game/asteroid_game_globals/asteroid_game_globals.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-## Autoloaded singleton that store game-wide references to important Nodes
-
-## Reference to the main player node of this game session
-var player_node : Node
--- Scenes/debug/debug_circle/debug_circle.gd
+++
@@ -1,9 +0,0 @@
-class_name DebugCircle
-extends Node2D
-
-@export var radius : float = 10
-
-@export var color : Color = Color.RED
-
-func _draw():
-	draw_circle(position, radius, color)
